f655a08fc27787b93ce80113c3a95a6dc2eeb339
craigryy <harriajames97@gmail.com>
2024-11-23 10:12:42 +0100

    initial commit

---
+++ delsu/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1ny4)_5u5&t0s^kj_d_n*%m9io#k&#ebu!o_v6^avuvx68^zrb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
