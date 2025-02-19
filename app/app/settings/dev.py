bcd56483e54327d1be965981c9c60ccefa30ead9
Craigryy <harria.hj@gmail.com>
2025-02-19 11:05:04 +0100

    added workflows

---
+++ app/app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7c6fe(91==&+dj_o6wmxl%-_kuz@it@s$4jj&3&1mwr-dxixd9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
